ce0c5f190a287cb3f92d1f1067741821a899657b
Jacques-on-Call <>
2025-10-03 20:59:36 +0000

    Sync updates from easy-seo editor directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx';
-import Callback from './Callback.jsx';
-import ExplorerPage from './ExplorerPage.jsx';
+import Test from './Test.jsx';
 import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/callback" element={<Callback />} />
-        <Route path="/explorer" element={<ExplorerPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Test />
   </React.StrictMode>,
 );
