a9c329127e8e138b2d9f57d848b614fc40fde786
Jacques-on-Call <>
2025-10-04 07:54:08 +0000

    Sync updates from easy-seo editor directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Test from './Test.jsx';
+import App from './App.jsx';
 import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Test />
+    <App />
   </React.StrictMode>,
 );
