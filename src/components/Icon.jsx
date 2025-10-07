3843384a5cd992769a56123939807d5833b9d804
Jacques-on-Call <>
2025-10-07 05:38:53 +0000

    Sync updates from easy-seo editor directory

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -28,7 +28,7 @@ const icons = {
   ),
   publish: (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-      <path strokeLinecap="round" strokeLinejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M22 2L11 13M22 2L15 22L11 13L2 9L22 2Z" />
     </svg>
   ),
   'arrow-left': (
