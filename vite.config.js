36c8945c04c4f67bfb1bff39dae5e4684cbe6ac7
Jacques-on-Call <>
2025-10-13 08:44:24 +0000

    Sync updates from easy-seo editor directory

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // The general proxy for all other API calls to the Wrangler server
       '/api': {
-        target: 'https://auth-edit-easy-seo.jacquesoncall.workers.dev',
+        target: 'http://localhost:8787',
         changeOrigin: true,
       },
     },
